56a635dd6dfb20cc8f6e754160c46b354e668ee9
Stephen <stephen.saliba@gmail.com>
2021-01-05 00:44:38 +0100

    Adds css to pagination

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,7 +1,38 @@
 import * as React from "react"
+import styled from "@emotion/styled"
+import { theme } from "../styles/theme"
 import { useQuery } from "@apollo/client"
 import { pageNumber } from "../graphql/cache"
 import { PAGE_NUMBER } from "../graphql/queries"
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 2rem;
+`
+const Button = styled.button`
+    width: 10rem;
+    height: 4rem;
+    margin: 2rem;
+    font-size: 1.2rem;
+    font-weight: 800;
+    color: ${(props) =>
+        props.disabled ? theme.colors.tableBorder : theme.colors.white};
+    background-color: ${(props) =>
+        props.disabled ? theme.colors.disabled : theme.colors.primary};
+    border-radius: 50rem;
+    border: none;
+    outline: none;
+    cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+`
+
+const Page = styled.span`
+    font-size: 2rem;
+    font-weight: 800;
+    color: ${theme.colors.text};
+    margin: 2rem;
+`
 
 export const Pagination: React.FunctionComponent = () => {
     const page = parseInt(useQuery(PAGE_NUMBER).data.pageNumber, 10)
@@ -11,14 +42,14 @@ export const Pagination: React.FunctionComponent = () => {
     }
 
     const handleDownClick = (): void => {
-        pageNumber(page + 1)
+        if (page > 0) pageNumber(page - 1)
     }
 
     return (
-        <>
-            <button onClick={handleUpClick}>{"<"}</button>
-            <span>{pageNumber()}</span>
-            <button onClick={handleDownClick}>{">"}</button>
-        </>
+        <Wrapper>
+            <Button onClick={handleDownClick}>{"<"}</Button>
+            <Page>{pageNumber() + 1}</Page>
+            <Button onClick={handleUpClick}>{">"}</Button>
+        </Wrapper>
     )
 }
